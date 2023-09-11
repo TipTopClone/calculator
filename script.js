82e2bb19598a1d8ce5081cde57ed6f074ca79ef7
TipTopClone <sujanmahat2468@gmail.com>
2023-09-11 11:49:08 +1000

    first update

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ let strToDisplay = '';
 const displayElm = document.querySelector('.display');
 
 const operators = ['+', '-', '*', '/', '%'];
+
+let lastOperator = '';
+
+const audio = new Audio('./prank.mp3');
 // yo muniko btn chai initialize gareko ho yesko thau ma j lekhda ni hunxa
 allBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
+    displayElm.style.background = '';
+    displayElm.style.color = '';
+    displayElm.classList.remove('prank');
+
     const val = btn.innerText;
     // console.log(val);
 
@@ -33,12 +41,27 @@ allBtns.forEach((btn) => {
     }
 
     if (operators.includes(val)) {
+      lastOperator = val;
       const lastChar = strToDisplay[strToDisplay.length - 1];
 
       if (operators.includes(lastChar)) {
         //remove the last char from the strToDisplay
 
         strToDisplay = strToDisplay.slice(0, -1);
+      }
+    }
+
+    if (val === '.') {
+      const indexOfLastOperator = strToDisplay.lastIndexOf(lastOperator);
+
+      const lastNumberSet = strToDisplay.slice(indexOfLastOperator);
+
+      if (lastNumberSet.includes('.')) {
+        return;
+      }
+
+      if (!lastOperator && strToDisplay.includes('.')) {
+        return;
       }
     }
 
@@ -52,7 +75,21 @@ const display = (str) => {
 };
 
 const total = () => {
+  const extraVal = randomNumber();
+  if (extraVal) {
+    audio.play();
+    displayElm.style.background = ' blue';
+    displayElm.style.color = 'yellow';
+    displayElm.classList.add('prank');
+  }
+
   const ttl = eval(strToDisplay);
-  display(ttl);
+  display(ttl + extraVal);
   strToDisplay = ttl.toString();
+};
+
+const randomNumber = () => {
+  const num = Math.round(Math.random() * 10);
+
+  return num < 3 ? num : 0;
 };
